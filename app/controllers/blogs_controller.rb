6979beb1d58e0ecd81327ec318825b811cbf861d
yutatanomoki <yuta3071@gmail.com>
2020-12-03 07:03:34 +0000

    [Start]3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,9 +25,13 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
-    
   end
   
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
   private
 
   def blog_params
